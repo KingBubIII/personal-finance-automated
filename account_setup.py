aab4753ad6be180aded3ab980ad6820c49de860b
KingBubIII <richardsoncaleb60@gmail.com>
2024-02-20 22:24:19 -0700

    created a decorator to update the config file and rolled the create_configs function into it

--- account_setup.py
+++ account_setup.py
@@ -3,8 +3,29 @@ from json import load, dumps
 from easygui import fileopenbox
 import csv_ops
 
+# decorator to easily add to configs file
+def update_configs(func):
+    def wrapper():
+        try:
+            # loads configs in python hash table
+            configs = load(open('configs.json','r'))
+        except FileNotFoundError:
+            configs = {"accounts": {}}
+        
+        # run function with json file contents passed
+        dict_return = func(configs)
+
+        # opens configs file in "write" mode, therefore empties configs file for overwritting later
+        with open('configs.json', 'w') as configs_writer:
+            # rewrites configs and prettifies output
+            configs_writer.write(dumps(dict_return, sort_keys=True, indent=4))
+
+        return True
+
+    return wrapper
+
 # check for a accepted answer
-def good_input_response(answer):
+def _good_input_response(answer):
     # make the user response case insensitive
     answer = answer.upper()
     # check for yes or no variations
@@ -13,70 +34,33 @@ def good_input_response(answer):
     else:
         return False
 
-def add_account():
-    # loads configs in python hash table
-    configs = load(open('configs.json','r'))
-
-    # opens configs file in "write" mode, therefore empties configs file for overwritting later
-    with open('configs.json', 'w') as configs_file:
-        # asks user input for account identifier and the path the CSV file
-        account_name = input('Account nickname: ')
-        csv_path = fileopenbox()
-        # automatically find columns with names similar to required names
-        print(csv_ops.show_csv_columns(csv_path))
-        columnIndexes = csv_ops.get_row_indexes( csv_path, {'date': -1, 'amount': -1, 'description': -1})
-        
-        # iterate though all column, index pairs
-        for col_name, col_index in columnIndexes.items():
-            response = ''
-            # ask user for input until input is valid
-            while not good_input_response(response):
-                if col_index == -1:
-                    response = input('Could not find a column for {0}, please select a column for it. '.format(col_name))
-                else:
-                    response = input('Is {0} the correct column index for {1}?'.format(col_index, col_name))
-            
-            if response == "YES" or response == "Y":
-                continue
+@update_configs
+def add_account(configs):
+    # asks user input for account identifier and the path the CSV file
+    account_name = input('Account nickname: ')
+    csv_path = fileopenbox()
+    # automatically find columns with names similar to required names
+    print(csv_ops.show_csv_columns(csv_path))
+    columnIndexes = csv_ops.get_row_indexes( csv_path, {'date': -1, 'amount': -1, 'description': -1})
+    
+    # iterate though all column, index pairs
+    for col_name, col_index in columnIndexes.items():
+        response = ''
+        # ask user for input until input is valid
+        while not _good_input_response(response):
+            if col_index == -1:
+                response = input('Could not find a column for {0}, please select a column for it.\n'.format(col_name))
             else:
-                while not response.isnumeric():
-                    response= input('What column should it be for {0}'.format(col_name))
-                
-                columnIndexes[col_name] = int(response)
-
-        # adds newly created account details to configs "accounts" section
-        configs["accounts"][account_name] = {   "csv_path": csv_path, 
-                                                "columnIndexes": columnIndexes}
-
-        # rewrites configs with updated "accounts" info back to file
-        configs_file.write(dumps(configs, sort_keys=True, indent=4))
-
-def create_configs():
-    # checks if file exists already
-    default_configs = {"accounts": {}}
-    try:
-        with open('configs.json','x') as configs_file:
-            configs_file.write(dumps(   
-                                        default_configs, 
-                                        sort_keys=True, 
-                                        indent= 4
-                                    )
-                                )
-    except FileExistsError:
-        # get user input if file should be overwritten with default configs
-        user_response= input('File already exists, would you like to start fresh? Y/N\n')
-        good_response= good_input_response(user_response)
-        if good_response:
-            if user_response == "YES" or user_response == "Y":
-                remove('configs.json')
-                # recursively call function to create defaults now that original file is deleted
-                create_configs()
-            elif user_response == "NO" or user_response == "N":
-                print('Cancelled')
+                response = input('Is {0} the correct column index for {1}? Y/N\n'.format(col_index, col_name))
+        
+        if response == "YES" or response == "Y":
+            continue
         else:
-            # recursively call function to get user input agin if configs should be deleted
-            print('Not a valid answer\n')
-            create_configs()
+            while not response.isnumeric():
+                response= input('What column should it be for {0}? Y/N\n'.format(col_name))
+            
+            columnIndexes[col_name] = int(response)
+    
+    configs['accounts'][account_name] = {"csv_path": csv_path, "columnIndexes": columnIndexes}
 
-create_configs()
-add_account()
+    return configs
