55c37ce2912de965432de9ff1a463c63b8bf3e70
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-03-18 18:37:26 -0600

    created a new window view for the actual home screen of the app.

--- display.py
+++ display.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import QComboBox, QVBoxLayout, QWidget, QGridLayout
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
 import classes as cl
+from configs_ops import read_configs
 
 def attach_all_tables(accounts_class, display_window):
     for account_name, table_obj in accounts_class.tables.items():
@@ -38,6 +40,72 @@ def start_CSV_review():
     # show_table(main_window, all_accounts, all_accounts.tables["All Accounts"])
 
     # displays entire window and all objects attached
+    window.show()
+
+    return window
+
+def configs_setup_GUI():
+    default_margin = 25
+
+    # whole window and layout
+    window = QWidget()
+    layout = QGridLayout(window)
+
+    # area to select which accounts to view
+    account_selection_area = QGroupBox("Account Selection", window)
+    stats_area = QGroupBox("Stats", window)
+    budget_review_area = QGroupBox("Budget Review", window)
+    checkbox_area = QWidget(account_selection_area)
+    checkbox_area_layout = QGridLayout(checkbox_area)
+
+    # making it scrollable for when there is many accounts
+    scroll_obj = QScrollArea(account_selection_area)
+    scroll_obj.setWidget(checkbox_area)
+
+    for count, account in enumerate(read_configs()["accounts"]):
+        test = QCheckBox(account, checkbox_area)
+        test.move(default_margin, default_margin*count)
+
+    import_account_btn = QPushButton('Import Account CSV', account_selection_area)
+
+    def _resize(event):
+        account_selection_area.setGeometry( default_margin,
+                                            default_margin,
+                                            window.geometry().width()//2,
+                                            window.geometry().height()//3)
+        # account_selection_area.geometry().setLeft(default_margin)
+
+        stats_area.setGeometry(QRect(
+                                    QPoint(
+                                            default_margin,
+                                            account_selection_area.geometry().bottom()+default_margin
+                                            ),
+                                    QPoint(
+                                            account_selection_area.geometry().right(),
+                                            window.geometry().bottom()-default_margin
+                                            )
+                                    )
+                                )
+        budget_review_area.setGeometry(QRect(
+                                    QPoint(
+                                            account_selection_area.geometry().right()+default_margin,
+                                            default_margin
+                                            ),
+                                    QPoint(
+                                            window.geometry().right()-default_margin,
+                                            window.geometry().bottom()-default_margin
+                                            )
+                                    )
+                                )
+
+        import_account_btn.setGeometry(default_margin, default_margin, 150, default_margin)
+        scroll_obj.setGeometry(default_margin, import_account_btn.geometry().bottom()+default_margin, account_selection_area.geometry().width()-(default_margin*2), account_selection_area.geometry().height()-(default_margin*2))
+        checkbox_area.setGeometry(default_margin, import_account_btn.geometry().bottom()+default_margin, scroll_obj.geometry().width()-(default_margin*2), (default_margin*(count+1)) )
+
+    window.resizeEvent = _resize
+    # window.showMaximized()
+    # print(window.geometry().bottom())
+    # print(stats_area.geometry().bottom())
     window.show()
 
     return window
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import display
 # init all nessessary window objects that everything else will attach to
 app = QApplication()
 
-main_window = display.start_CSV_review()
+# main_window = display.start_CSV_review()
+main_window = display.configs_setup_GUI()
 
 sys.exit(app.exec())
